31921c18913c908ba683c1484252ca66eeac671d
Kasia Izak <kontakt@kasiaizak.pl>
2019-11-12 15:52:15 +0100

    change configuration

--- src/js/main.js
+++ src/js/main.js
@@ -14,9 +14,9 @@ browserUpdate({
 });
 
 const autor = 'Kasia Izak';
-
 console.log(`Hej, nazywam się ${autor} i jestem autorką tej strony.
 Fajnie, że zaglądasz do konsoli, ale czego się tu spodziewasz znaleźć? ;-)`);
+
 
 const footerParagraph = document.querySelector('.footer__paragraph--js');
 
